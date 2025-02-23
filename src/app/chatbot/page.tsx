7dd1257a6aa9d1f67b15bcfc3031c4dedcb6fd02
Parsa Bazrpash Amalgar <148006143+ParsaBazrpash@users.noreply.github.com>
2025-02-23 02:56:18 -0600

    Update page.tsx

--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, JSX} from 'react';
 import { Send, ArrowLeft } from 'lucide-react';
 import Link from 'next/link';
 import AnimatedMessage from '../components/AnimatedMessage';
